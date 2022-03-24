30215bd1204c3f68278ed79e39aa34ad87b0dfc7
GregoryDesarrollo01 <Desarrollo01@patridgeconsulting.com>
2022-03-24 11:25:30 -0400

    a

--- prueba.py
+++ prueba.py
@@ -1,2 +1,27 @@
 #:git cambios
-print('hello word para todos')
+
+
+null_variable = None
+not_null_variable = 'Hello There!'
+ 
+# The is keyword
+if null_variable is []:
+    print('null_variable is None')
+else:
+    print('null_variable is not None')
+ 
+if not_null_variable is None:
+    print('not_null_variable is None')
+else:
+    print('not_null_variable is not None'
+ 
+# The == operator
+if null_variable == None:
+    print('null_variable is None aqui')
+else:
+    print('null_variable is not None no aqui')
+ 
+if not_null_variable != None:
+    print('not_null_variable is None aqui')
+else:
+    print('not_null_variable is not None no aqui')
